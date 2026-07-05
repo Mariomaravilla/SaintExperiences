bc04b87d50407cb8d1e53492a5700ec062daec9f
Mario <20230615@ricaldone.edu.sv>
2026-07-04 23:33:41 -0600

    hola

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace SaintExperiences
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
-        //hola
+        //hola que tal?
 
     }
 }
