49082271e443df48332e27014dee71c4d6194fbb
Chris <Chris.quijano008@gmail.com>
2026-07-07 13:49:58 -0600

    Carpetas

---
+++ Modelos/Conexion_DB/Conexion_DB.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelos.Conexion_DB
+{
+    internal class Conexion_DB
+    {
+    }
+}
